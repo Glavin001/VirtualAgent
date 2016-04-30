6d9d16ea034c7ab87f2ed46c31d39259ccb6139a
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-04-30 16:40:15 -0300

    Move tags.json to data, add resume template file

--- scripts/stackoverflowtags.py
+++ scripts/stackoverflowtags.py
@@ -3,15 +3,15 @@ Author: Conor Scott
 Date: 04/29/16
 Volta Hackathon
 
-Purpose: Unique tags (Like Java) from stackoverflow are obtained from 
-the json file I copied from their website API that obtains 
+Purpose: Unique tags (Like Java) from stackoverflow are obtained from
+the json file I copied from their website API that obtains
 them 100 at a time, and compiled 1000 of the most popular
 tags
 
 Requirements: just python (comes with json)
 """
 import json
-jsonObject = json.load(open("tags.json", 'r'))
+jsonObject = json.load(open("data/tags.json", 'r'))
 tags = set()
 print jsonObject
 for dictionary in jsonObject['items']:
