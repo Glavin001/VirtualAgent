1a9e480875c573712299342b807fec790607952a
Conor Scott <conorwscott93@gmail.com>
2016-04-30 15:44:24 -0300

    Added ability to get skills from resume

---
+++ scripts/ResumeSkillLoader.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+def loadResume(filename):
+	p = os.path.abspath('..')
+	jsonObject = json.load(open(p + filename, 'r'))
+	return jsonObject
+
+def getSkills(resume):
+	return resume['skills']
+
+def getUniqueSkills(skills):
+	a = set()
+	for keyword in skills: 
+		for item in keyword['keywords']:
+			a.add(item)
+	print a
+
+#Get Aaron's Skills from his resume
+getUniqueSkills(getSkills(loadResume("\data\\resumes\\aaron.json")))
