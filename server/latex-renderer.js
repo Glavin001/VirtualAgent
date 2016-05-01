3825f9a663adfcf7cc2d17f6d87c955030025191
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-05-01 16:27:19 -0300

    See #1. Generate Resume PDF

--- server/latex-renderer.js
+++ server/latex-renderer.js
@@ -7,108 +7,6 @@ const _ = require('lodash');
 const mkdirp = require('mkdirp');
 const exec = require('child_process').exec;
 
-let user = {
-  "basics": {
-    "name": "John Doe",
-    "label": "Programmer",
-    "picture": "",
-    "email": "john@gmail.com",
-    "phone": "(912) 555-4321",
-    "website": "http://johndoe.com",
-    "summary": "A summary of John Doe...",
-    "location": {
-      "address": "2712 Broadway St",
-      "postalCode": "CA 94115",
-      "city": "San Francisco",
-      "countryCode": "US",
-      "region": "California"
-    },
-    "profiles": [{
-      "network": "Twitter",
-      "username": "john",
-      "url": "http://twitter.com/john"
-    }]
-  },
-  "work": [{
-    "company": "Company",
-    "position": "President",
-    "website": "http://company.com",
-    "startDate": "2013-01-01",
-    "endDate": "2014-01-01",
-    "summary": "Description...",
-    "highlights": [
-      "Started the company"
-    ]
-  }],
-  "volunteer": [{
-    "organization": "Organization",
-    "position": "Volunteer",
-    "website": "http://organization.com/",
-    "startDate": "2012-01-01",
-    "endDate": "2013-01-01",
-    "summary": "Description...",
-    "highlights": [
-      "Awarded 'Volunteer of the Month'"
-    ]
-  }],
-  "education": [{
-    "institution": "University",
-    "area": "Software Development",
-    "studyType": "Bachelor",
-    "startDate": "2011-01-01",
-    "endDate": "2013-01-01",
-    "gpa": "4.0",
-    "courses": [
-      "DB1101 - Basic SQL"
-    ]
-  }],
-  "awards": [{
-    "title": "Award",
-    "date": "2014-11-01",
-    "awarder": "Company",
-    "summary": "There is no spoon."
-  }],
-  "publications": [{
-    "name": "Publication",
-    "publisher": "Company",
-    "releaseDate": "2014-10-01",
-    "website": "http://publication.com",
-    "summary": "Description..."
-  }],
-  "skills": [{
-    "name": "Web Development",
-    "level": "Master",
-    "keywords": [
-      "HTML",
-      "CSS",
-      "Javascript"
-    ]
-  }],
-  "languages": [{
-    "name": "English",
-    "level": "Native speaker"
-  }],
-  "interests": [{
-    "name": "Wildlife",
-    "keywords": [
-      "Ferrets",
-      "Unicorns"
-    ]
-  }],
-  "references": [{
-    "name": "Jane Doe",
-    "reference": "Reference..."
-  }]
-};
-let job = {
-  'title': 'Front End Web Developer',
-  'source': 'GitHub Careers',
-  'company_name': 'GitHub'
-};
-let context = {
-  user,
-  job
-};
 
 const renderTemplate = (templatePath, context) => {
   return new Promise((resolve, reject) => {
@@ -177,8 +75,8 @@ const renderCoverLetter = (context) => {
     .then(([intro, company, skills, work, volunteer, thanks]) => {
       // Render 7 - Complete Cover Letter
       return renderTemplate('cover-letter/template.tex', {
-        user,
-        job,
+        user: context.user,
+        job: context.job,
         paragraphs: {
           intro,
           company,
@@ -193,7 +91,47 @@ const renderCoverLetter = (context) => {
 };
 
 
-const generateCoverLetter = (username, context) => {
+const renderResume = (context) => {
+  return Promise.all([
+      // // Render 1 - Intro
+      // renderTemplate('cover-letter/1-intro/*', context),
+      // // renderTemplate('cover-letter/1-intro/1.tex', context),
+      // // Render 2 - Company Fit
+      // renderTemplate('cover-letter/2-company_fit/*', context),
+      // // renderTemplate('cover-letter/2-company_fit/1.tex', context),
+      // // Render 3 - Skills & Projects
+      // renderTemplate('cover-letter/3-skills_projects/*', context),
+      // // renderTemplate('cover-letter/3-skills_projects/1.tex', context),
+      // // Render 4 - Work Fit
+      // renderTemplate('cover-letter/4-work_fit/*', context),
+      // // renderTemplate('cover-letter/4-work_fit/1.tex', context),
+      // // Render 5 - Volunteer
+      // renderTemplate('cover-letter/5-volunteer/*', context),
+      // // renderTemplate('cover-letter/5-volunteer/1.tex', context),
+      // // Render 6 - Thanks
+      // renderTemplate('cover-letter/6-thanks/*', context),
+      // // renderTemplate('cover-letter/6-thanks/1.tex', context),
+    ])
+    .then(([intro, company, skills, work, volunteer, thanks]) => {
+      // Render 7 - Complete Cover Letter
+      return renderTemplate('resume/template.tex', {
+        user: context.user,
+        job: context.job,
+        // paragraphs: {
+        //   intro,
+        //   company,
+        //   skills,
+        //   work,
+        //   volunteer,
+        //   thanks
+        // }
+      });
+    });
+
+};
+
+
+const generatePDF = (username, filename, renderFn, context) => {
   let userDir = path.resolve(__dirname, '../build/', username);
 
   return new Promise((resolve, reject) => {
@@ -209,14 +147,14 @@ const generateCoverLetter = (username, context) => {
     })
     .then(() => {
       // Generate Cover Letter
-      console.log('Generating cover letter');
-      return renderCoverLetter(context);
+      console.log('Generating '+filename);
+      return renderFn(context);
     })
     .then((coverLetter) => {
       console.log('Have cover letter!');
       return new Promise((resolve, reject) => {
         // Write cover letter to file
-        let coverLetterPath = path.resolve(userDir, 'cover-letter.tex');
+        let coverLetterPath = path.resolve(userDir, filename);
         fs.writeFile(coverLetterPath, coverLetter, (err) => {
           if (err) {
             return reject(err);
@@ -253,10 +191,18 @@ const generateCoverLetter = (username, context) => {
 
 };
 
-generateCoverLetter('john-doe', context)
-.then(() => {
-  console.log('Done!');
-})
-.catch((error) => {
-  console.log(error);
-});
+const generateCoverLetter = (username, context) => {
+  return generatePDF(username, 'cover-letter.tex', renderCoverLetter, context);
+};
+
+const generateResume = (username, context) => {
+  return generatePDF(username, 'resume.tex', renderResume, context);
+};
+
+
+module.exports = {
+  renderTemplate,
+  renderCoverLetter,
+  generateCoverLetter,
+  generateResume,
+};
