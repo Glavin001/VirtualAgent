2e2b37514808c8018fd84d0c3c4a9a9dc0d121c6
ChaosSmith <zv2.2smith@gmail.com>
2016-04-30 21:49:43 -0300

    udated ES test_file

--- README.md
+++ README.md
@@ -3,7 +3,17 @@
 
 ## Installation
 
-Clone this repository!
+1. Clone this repository!
+2. `pip install -r requirements.txt`
+3. Profit
+
+## Usage
+
+```bash
+python server
+```
+
+Then go to [http://127.0.0.1:5000/](http://127.0.0.1:5000/).
 
 ## Contributing
 
--- scripts/elasticsearch_test.py
+++ scripts/elasticsearch_test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from elasticsearch import Elasticsearch
 import urllib2
 import requests
@@ -29,3 +30,43 @@ for i in range(len(data)):
 	es.index(index='github_jobs', doc_type='test-type',id = i, body = data[i])	
 
 print es.search(index='github_jobs', body={"query": {"match": {'description':'interest'}}})
+=======
+from datetime import datetime
+from elasticsearch_dsl import DocType, String, Date, Integer
+from elasticsearch_dsl.connections import connections
+
+# Define a default Elasticsearch client
+connections.create_connection(hosts=['localhost'])
+
+class Article(DocType):
+    title = String(analyzer='snowball', fields={'raw': String(index='not_analyzed')})
+    body = String(analyzer='snowball')
+    tags = String(index='not_analyzed')
+    published_from = Date()
+    lines = Integer()
+
+    class Meta:
+        index = 'blog'
+
+    def save(self, ** kwargs):
+        self.lines = len(self.body.split())
+        return super(Article, self).save(** kwargs)
+
+    def is_published(self):
+        return datetime.now() > self.published_from
+
+# create the mappings in elasticsearch
+Article.init()
+
+# create and save and article
+article = Article(meta={'id': 42}, title='Hello world!', tags=['test'])
+article.body = ''' looong text '''
+article.published_from = datetime.now()
+article.save()
+
+article = Article.get(id=42)
+print(article.is_published())
+
+# Display cluster health
+print(connections.get_connection().cluster.health())
+
