78eac34e5e5d966c05d5ba451b659a04f40bbff9
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-04-30 22:26:08 -0300

    See #8. Improve background video, transition between tabs, add spinner

--- public/main.js
+++ public/main.js
@@ -1,1 +1,16 @@
 console.log('Simply Sift!');
+
+$('.sync-github-btn').click(function(event) {
+  // Show tab
+  $('.tab-content .tab-pane').removeClass('active in');
+  $('.tab-content .tab-pane#syncing').addClass('active in');
+
+  // Fetch
+  fetch('/api/user/glavin001', { method: 'post' })
+  .then(function(res) {
+    console.log(res);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+});
