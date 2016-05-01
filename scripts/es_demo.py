fda05928294cb838300014566bc9d28d3130a543
ChaosSmith <zv2.2smith@gmail.com>
2016-05-01 01:33:20 -0300

    added a unified JSON file combined_jobs.json

--- scripts/es_demo.py
+++ scripts/es_demo.py
@@ -27,15 +27,21 @@ with open(PATH_TO_DATA + 'StackOverflow_Jobs.json') as data_file:
 
 combined_jobs = GH_data + SO_data
 
+# Export Combined data for later
+
+with open(PATH_TO_DATA + 'Combined_Jobs.json', 'w') as outfile:
+
+    json.dump(combined_jobs, outfile)
+
 # Index Data
 
-# Only need to run indexing code ONCE, uncomment it
+# Only need to run indexing code ONCE, uncomment it if needed: be warned 66 Second run time
 
 #for i in range(len(combined_jobs)):
 
 	#es.index(index='combined_jobs', doc_type='test-type',id = i, body = combined_jobs[i])
 
-# Example Query
+# Example Query + returns highest scoring job's post formated as specified
 
 print combined_jobs[int(es.search(index='combined_jobs', body ={"query": {"match" : { "description": "C++ AND Java"}}})['hits']['hits'][0]['_id'].encode('utf-8'))]
 
